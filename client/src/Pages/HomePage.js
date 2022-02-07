68618637074c7fbaa3e44dbf7b34469c388d3e92
guanch92 <guancheng92@gmail.com>
2022-02-07 19:33:09 +0800

    gc node modules pushed to repo to sync, client src folder set up, instructions and README updated

---
+++ client/src/Pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { TestHeader } from '../components/HomePage/TestHeader';
+
+const HomePage = () => {
+  return <div>
+      <TestHeader />
+  </div>;
+};
+    export default HomePage;
---
+++ client/src/components/HomePage/TestHeader.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const TestHeader = () => {
+  return <div>
+      <h1>Welcome to CRA - This is a Test Header Component For HomePage</h1>
+  </div>;
+};
