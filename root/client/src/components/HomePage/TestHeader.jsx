b8ec024e6dfe843f6db346109ca238225248ef8c
guanch92 <guancheng92@gmail.com>
2022-02-07 20:08:40 +0800

    installed tailwind css

--- root/client/src/components/HomePage/TestHeader.jsx
+++ root/client/src/components/HomePage/TestHeader.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export const TestHeader = () => {
   return <div>
-      <h1>Welcome to CRA - This is a Test Header Component For HomePage</h1>
+      <h1 className='font-sans text-3xl font-bold text-red-700'>Welcome to CRA - This is a Test Header Component For HomePage</h1>
+      <p className='font-sans text-xl font-semibold text-blue-700'>Styling by Tailwind CSS</p>
   </div>;
 };
