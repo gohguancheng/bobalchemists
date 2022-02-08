257001b25bad32c17d948bee67362ab78cd542f1
guanch92 <guancheng92@gmail.com>
2022-02-08 13:00:58 +0800

    gc set up navbar and homepage, installed react-router-dom

--- root/client/src/App.js
+++ root/client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Reusables/Navbar';
 import HomePage from './Pages/HomePage';
 //* import pages here and plugin pages as components inside retun below
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div className="App font-sans h-screen w-screen">
+      <Navbar />
+      <Routes>
+    <Route path="/" element={<HomePage />} />
+    {/* <Route path="/TEMPLATE" element={COMPONENT} /> */}
+      </Routes>
     </div>
   );
 }
