1e479b054227e178ca2835d35f7a5eccaa81c5a5
guanch92 <guancheng92@gmail.com>
2022-02-09 10:53:34 +0800

    FilterSection: created simple toggleHandler to change query output state

--- root/client/src/components/Reusables/Card.jsx
+++ root/client/src/components/Reusables/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({ info }) => {
     <div className="flex flex-col justify-center items-center gap-4">
       <div className="basis-40">Graphic Art</div>
       <div className="h-max">{info.name}</div>
-      <div className="flex flex-row gap-4 justify-center items-center">{ingredients}</div>
+      <div className="flex flex-row gap-4 text-sm justify-center items-center">{ingredients}</div>
       <span className="flex flex-row gap-4 justify-center items-center">
       <div>Likes:{info.likes}</div>
       <div>Created By: {info.creator}</div>
