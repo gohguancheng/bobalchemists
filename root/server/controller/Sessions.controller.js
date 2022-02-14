163286e861f7471d08c3b70f5defa517acb51652
guanch92 <guancheng92@gmail.com>
2022-02-14 14:29:47 +0800

    created user schema, model, new user creation routes and authentication routes

---
+++ root/server/controller/Sessions.controller.js
@@ -0,0 +1,67 @@
+// DEPENDENCIES
+const bcrypt = require("bcrypt");
+const express = require("express");
+const sessions = express.Router();
+const User = require('../models/userData.js');
+
+// ROUTES (api/sessions/)
+// get 'api/sessions/login' index
+sessions.get("/login", async (req, res) => {
+    res.send("WELCOME TO LOGIN!")
+  try {
+    const foundUser = await User.findOne({ username: req.body.username });
+    if (!foundUser) {
+      res.status(200).json({
+        message: "Sorry, no user found",
+        isAuthenticated: false,
+        status: "Login Failure",
+      });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// post 'api/sessions/login' index ---> checks login credentials
+sessions.post("/login", async (req, res) => {
+  try {
+    const foundUser = await User.findOne({ username: req.body.username });
+    if (!foundUser) {
+      res.status(200).json({
+        message: "Sorry, no user found",
+        isAuthenticated: false,
+        status: "Login Failure",
+      });
+    } else {
+      if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+        req.session.currentUser = foundUser;
+        res.status(200).json({
+          message: "Login successful",
+          isAuthenticated: true,
+          status: "Login Success",
+        });
+      } else {
+        res.status(200).json({
+          message: "password does not match",
+          isAuthenticated: false,
+          status: "Login Failure",
+        });
+      }
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// get 'api/sessions/logout'
+sessions.get("/logout", async (req, res) => {
+  req.session.destroy(() => {
+    res.status(200).json({
+      message: "Logout successful",
+      isAuthenticated: false,
+      status: "Logout Success",
+    });
+  });
+});
+
+module.exports = sessions;
--- root/server/models/userData.js
+++ root/server/models/userData.js
@@ -11,6 +11,6 @@ const userSchema = new Schema(
     { timestamps: true }
 )
 
-const User = mongoose.model("user", userSchema)
+const User = mongoose.model("users", userSchema)
 
 module.exports = User;
