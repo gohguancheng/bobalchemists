4f47ff5dd1299212edb65a153d5559343e67a934
rizalmab <rizalmahadiab@gmail.com>
2022-02-14 12:22:09 +0800

    Updated AvailableIngredients component

--- root/client/src/components/CreatePage/AvailableIngredients.jsx
+++ root/client/src/components/CreatePage/AvailableIngredients.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const AvailableIngredients = () => {
   return (
     <div className="container mx-auto h-screen w-1/2 text-center justify-around">
-      Testing
+      <h1>Available ingredients</h1>
+      <div>
+        <img
+          alt="ingredient"
+          src="https://img.lovepik.com/element/40108/6382.png_300.png"
+        />
+        <p>Milk tea</p>
+      </div>
     </div>
   );
 };
