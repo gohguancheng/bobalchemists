bb36cb13067e5211d93fd711e55c8e76d1862aa5
rizalmab <rizalmahadiab@gmail.com>
2022-02-14 14:12:05 +0800

    Added in svg images for BBT

--- root/client/src/Pages/CreatePage.js
+++ root/client/src/Pages/CreatePage.js
@@ -6,9 +6,21 @@ const CreatePage = () => {
   return (
     <div className="flex">
       <div className="container inline-block w-1/4 h-screen">
-        <img
+        {/* <img
           src="https://image.shutterstock.com/image-vector/cute-boba-bubble-green-tea-600w-793882456.jpg"
           alt="bbt"
+        /> */}
+        <img
+          src="https://drive.google.com/uc?export=view&id=1DIvoIC4JTWbeTzwiouiikbe81dmu8VcE"
+          alt="cup"
+        />
+        <img
+          src="https://drive.google.com/uc?export=view&id=1pgEENjWjQldeA95EyXCXcemZeaNKbMs2"
+          alt="grass jelly"
+        />
+        <img
+          src="https://drive.google.com/uc?export=view&id=1KULwHaygOZFVPkRnzVkYj4zItod4AZhn"
+          alt="milk tea"
         />
       </div>
       <div className="container w-3/4 h-screen flex">
