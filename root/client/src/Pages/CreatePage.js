22d860f260d6b3cdb7999b43afb0faa8b477bade
rizalmab <rizalmahadiab@gmail.com>
2022-02-14 14:29:48 +0800

    Completed overlay of images for BBT

--- root/client/src/Pages/CreatePage.js
+++ root/client/src/Pages/CreatePage.js
@@ -5,22 +5,23 @@ import AvailableIngredients from "../components/CreatePage/AvailableIngredients"
 const CreatePage = () => {
   return (
     <div className="flex">
-      <div className="container inline-block w-1/4 h-screen">
-        {/* <img
-          src="https://image.shutterstock.com/image-vector/cute-boba-bubble-green-tea-600w-793882456.jpg"
-          alt="bbt"
-        /> */}
+      <div className="container inline-block w-1/4 h-screen relative">
+        {/* Put BASE here so it will appear above CUP but below TOPPING */}
         <img
-          src="https://drive.google.com/uc?export=view&id=1DIvoIC4JTWbeTzwiouiikbe81dmu8VcE"
-          alt="cup"
+          className="absolute"
+          src="https://drive.google.com/uc?export=view&id=1KULwHaygOZFVPkRnzVkYj4zItod4AZhn"
+          alt="milk tea"
         />
+        {/* Put TOPPING here so it will appear above BASE */}
         <img
+          className="absolute"
           src="https://drive.google.com/uc?export=view&id=1pgEENjWjQldeA95EyXCXcemZeaNKbMs2"
           alt="grass jelly"
         />
+        {/* Put CUP here so it will appear below BASE and TOPPING */}
         <img
-          src="https://drive.google.com/uc?export=view&id=1KULwHaygOZFVPkRnzVkYj4zItod4AZhn"
-          alt="milk tea"
+          src="https://drive.google.com/uc?export=view&id=1DIvoIC4JTWbeTzwiouiikbe81dmu8VcE"
+          alt="cup"
         />
       </div>
       <div className="container w-3/4 h-screen flex">
