25fd7f3b4c13f1e0c95b98b23e45b7b356a223d5
guanch92 <guancheng92@gmail.com>
2022-02-15 12:05:48 +0800

    seed users, switched to axios, troubleshooting login/signup

--- root/client/src/components/HomePage/Gallery.jsx
+++ root/client/src/components/HomePage/Gallery.jsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import Card from '../Reusables/Card';
 import { cardInfo } from './seedCards';
+const axios = require("axios").default;
 
 const Gallery = () => {
     const [gallery, setGallery] = useState(cardInfo);
+    
+    
+
     const galleryDisplay = gallery.map(e=><Link key={e._id} to={`/${e._id}`}><Card key={e._id} info={e} />/</Link>);
   return (
-  <div className='p-4 flex inline-flex gap-4'>
+  <div className='p-4 flex gap-4'>
       {galleryDisplay}
   </div>)
 }
