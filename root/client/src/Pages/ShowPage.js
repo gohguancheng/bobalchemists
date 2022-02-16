df7bcc84cfd862099cd1f789027a9db8a5569dbd
rizalmab <rizalmahadiab@gmail.com>
2022-02-16 11:16:50 +0800

    displayed ingredients

--- root/client/src/Pages/ShowPage.js
+++ root/client/src/Pages/ShowPage.js
@@ -12,8 +12,9 @@ const ShowPage = () => {
 
   useEffect(async () => {
     await axios.get(`/api/teacardsinfo/show/${id}`).then((res) => {
-      console.log("res", res);
-      // setCard();
+      console.log("res", res.data.data);
+      setCard(res?.data?.data);
+      console.log("Card data", card);
     });
   }, []);
 
@@ -22,16 +23,13 @@ const ShowPage = () => {
       <CreatedImage />
       <div className="container inline-block w-2/3 h-screen font-normal">
         <div className="container mx-auto w-5/6 h-screen p-20 text-center justify-around">
-          <h1 className="text-2xl py-5">Caramel Milk Tea Delight</h1>
-          <p className="py-5">
-            Classic milk tea with a dash of caramel to bring some delight to
-            your day!
-          </p>
+          <h1 className="text-2xl py-5">{card.name}</h1>
+          <p className="py-5">{card.description}</p>
           <div className="py-5">
             <h1>Ingredients</h1>
-            <p>Base:</p>
-            <p>Flavouring:</p>
-            <p>Toppings:</p>
+            <p>Base: {card?.base?.name}</p>
+            <p>Flavouring: {card?.flavour?.name}</p>
+            <p>Toppings:{card?.toppings}</p>
           </div>
           <div>
             <p className="w-1/3 inline-block">👍 Likes</p>
