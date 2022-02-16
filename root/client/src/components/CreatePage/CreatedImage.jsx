ac98fbdae7ab12a351d09dc0c8da41499d8ae2bf
rizalmab <rizalmahadiab@gmail.com>
2022-02-16 22:06:25 +0800

    Displayed topping

--- root/client/src/components/CreatePage/CreatedImage.jsx
+++ root/client/src/components/CreatePage/CreatedImage.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 
 const CreatedImage = ({ info }) => {
-  console.log("info", info);
+  console.log("info", info?.toppings);
   return (
     <div className="container inline-block w-1/4 h-1/3 relative bg-lightgray rounded-full drop-shadow-md">
       {/* Put BASE here */}
       <img className="absolute" src={info?.base?.img} alt="base" />
 
       {/* Put TOPPING here*/}
-      <img
+      {/* <img
         className="absolute"
         src="https://drive.google.com/uc?export=view&id=1pgEENjWjQldeA95EyXCXcemZeaNKbMs2"
         alt="topping(s)"
-      />
-      {/* {info?.toppings.map((topping) => {
-        <img
-          className="absolute"
-          src="https://drive.google.com/uc?export=view&id=1pgEENjWjQldeA95EyXCXcemZeaNKbMs2"
-          alt="topping(s)"
-        />;
-      })} */}
+      /> */}
+      {info?.toppings.map((topping) => {
+        return <img className="absolute" src={topping.img} alt="topping(s)" />;
+      })}
       {/* Put FLAVOURING here */}
       {info?.flavour?.img &&
       info?.flavour?._id !== "620b2afdb98e26a5d939f8d8" ? (
