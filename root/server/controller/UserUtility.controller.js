cb6f504adb4d6a1e18973f592375dcfbd3c58096
donuts030 <ye.danning@gmail.com>
2022-02-17 14:00:37 +0800

    added checks to the likes route

---
+++ root/server/controller/UserUtility.controller.js
@@ -0,0 +1,29 @@
+const bcrypt = require("bcrypt");
+const express = require("express");
+const Router = express.Router();
+const User = require("../models/userData.js");
+
+//get 'api/userdata/' ---> route only accesible by admin
+
+//get 'api/userdata/admin/:id' ---> for listing admins
+
+//put 'api/userdata/permissions/:id' ---> toggle permissons: only assessible by admin
+
+//get 'api/userdata/creations/:id' ---> to populate and show creations
+
+//get 'api/userdata/liked/:id'---> to populate and show liked
+
+
+//put 'api/userdata/edit/:id' ---> route is called for users to update name and other stuff
+/* Router.put("/edit/:id", async (req, res) => {
+    try{
+        res.status(200).json({
+            status:"ok",
+            message: "found and updated",
+            data: findUpdateUser,
+        })
+        
+    }catch(error){
+        console.log("at /updateuserdata/newteacard/:id", error);
+    }
+}); */
