3aeaed7c157fd9d1b878fb26a5edcfb39d9bac9f
rizalmab <rizalmahadiab@gmail.com>
2022-02-17 15:38:56 +0800

    Added hover on Cards and popular and recently added buttons

--- root/client/src/components/Reusables/Card.jsx
+++ root/client/src/components/Reusables/Card.jsx
@@ -3,24 +3,26 @@ import { useEffect } from "react";
 import CreatedImage from "../CreatePage/CreatedImage";
 const axios = require("axios").default;
 
-const updateDBLikes = () => {
-
-}
+const updateDBLikes = () => {};
 
 const Card = ({ info }) => {
   console.log(info);
 
-  const handleLikeClick = () => {
+  const handleLikeClick = () => {};
 
-  }
-  
   return (
-    <div className="flex flex-col justify-center items-center gap-4 bg-white rounded-lg w-80 drop-shadow-md">
+    <div className="flex flex-col justify-center items-center gap-4 bg-white rounded-lg w-80 drop-shadow-md hover:border-purple hover:border-2">
       <CreatedImage info={info} />
       <div className="h-max text-center text-sm font-semibold">{info.name}</div>
       <span className="flex flex-row gap-4 justify-between items-center">
-        <div className="text-left text-xs w-max" > <button onClick={handleLikeClick} >👍</button><span> Likes: {info.likes} </span></div>
-        <div className="text-right text-xs w-max">Created By: {info.createdBy}</div>
+        <div className="text-left text-xs w-max">
+          {" "}
+          <button onClick={handleLikeClick}>👍</button>
+          <span> Likes: {info.likes} </span>
+        </div>
+        <div className="text-right text-xs w-max">
+          Created By: {info.createdBy}
+        </div>
       </span>
     </div>
   );
