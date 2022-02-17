16503afd454db10a7e866352429f37f4bae16c3d
guanch92 <guancheng92@gmail.com>
2022-02-17 11:05:04 +0800

    styling sign up page

--- root/client/src/components/LoginPage/SignUpForm.jsx
+++ root/client/src/components/LoginPage/SignUpForm.jsx
@@ -140,7 +140,7 @@ const SignUpForm = ({ setAccessSignUp }) => {
               <input
                   name="testerpass"
                   type="text"
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 w-40 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Tester passcode"
                   onChange={(e) => setKeyInput(e.target.value)}></input> <p className="text-xs">App in beta: Please ask admins for passcode to complete sign up.</p> 
             </div> }
