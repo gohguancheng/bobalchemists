e212a9436ad4122f154a7fed2873e3c706970e1c
rizalmab <rizalmahadiab@gmail.com>
2022-02-17 14:34:21 +0800

    Added styling to show page

--- root/client/src/Pages/ShowPage.js
+++ root/client/src/Pages/ShowPage.js
@@ -45,37 +45,42 @@ const ShowPage = ({ currentUsername }) => {
       <CreatedImage info={card} />
       <div className="container inline-block w-2/3 h-screen font-normal">
         <div className="container mx-auto w-5/6 h-screen p-20 text-center justify-around">
-          <h1 className="text-2xl py-5">{card?.name}</h1>
-          <p className="py-5">{card?.description}</p>
-          <div className="py-5">
-            <h1>Ingredients</h1>
-            <p>Base: {card?.base?.name}</p>
-            <p>Flavouring: {card?.flavour?.name}</p>
-            <p>Toppings: {card?.toppings?.map((e) => e.name).join(", ")}</p>
-          </div>
-          <div>
-            <p className="w-1/3 inline-block">👍 {card?.likes} Likes</p>
-            <p className="w-1/3 inline-block">Creator: {card?.createdBy}</p>
-            {rightsToEdit ? (
-              <div>
-                <Button color="primary" onClick={() => handleEditButtonClick()}>
-                  Edit
-                </Button>
-                <Button
-                  color="danger"
-                  onClick={() => handleDeleteButtonClick()}
-                >
-                  Delete
-                </Button>
-              </div>
-            ) : !currentUsername ? (
-              <div>
-                <span className="text-xs">
-                  {" "}
-                  For creators, log in for more functions.
-                </span>
-              </div>
-            ) : null}
+          <div className="bg-white rounded-lg drop-shadow-lg p-3 pb-3">
+            <h1 className="text-2xl py-5">{card?.name}</h1>
+            <p className="py-5">{card?.description}</p>
+            <div className="py-5">
+              <h1>Ingredients</h1>
+              <p>Base: {card?.base?.name}</p>
+              <p>Flavouring: {card?.flavour?.name}</p>
+              <p>Toppings: {card?.toppings?.map((e) => e.name).join(", ")}</p>
+            </div>
+            <div>
+              <p className="w-1/3 inline-block">👍 {card?.likes} Likes</p>
+              <p className="w-1/3 inline-block">Creator: {card?.createdBy}</p>
+              {rightsToEdit ? (
+                <div>
+                  <Button
+                    color="primary"
+                    onClick={() => handleEditButtonClick()}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    color="danger"
+                    onClick={() => handleDeleteButtonClick()}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              ) : !currentUsername ? (
+                <div>
+                  <span className="text-xs">
+                    {" "}
+                    For creators, log in for more functions.
+                  </span>
+                </div>
+              ) : null}
+            </div>
           </div>
         </div>
       </div>
