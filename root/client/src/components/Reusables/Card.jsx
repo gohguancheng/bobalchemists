a84c29b096ddb43d964e1eab185d3c52aca31bac
guanch92 <guancheng92@gmail.com>
2022-12-06 22:20:43 +0800

    update card

--- root/client/src/components/Reusables/Card.jsx
+++ root/client/src/components/Reusables/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CreatedImage from "../CreatePage/CreatedImage";
 const axios = require("axios").default;
 
@@ -23,6 +23,8 @@ const Card = ({ info, currentUserData }) => {
   });
   const [likedByCurrent, setLikedByCurrent] = useState();
   const [user, setUser] = useState();
+  const [hover, setHover] = useState(false);
+  const navigate = useNavigate();
 
   //set up local user data --> for page refreshes
   useEffect(async () => {
@@ -53,7 +55,8 @@ const Card = ({ info, currentUserData }) => {
   }, [user]);
 
   const handleLikeClick = async (e) => {
-    e.preventDefault();
+    e.stopPropagation();
+
     if (!currentUserData || !info) {
       return;
     }
@@ -86,50 +89,63 @@ const Card = ({ info, currentUserData }) => {
     }
   };
 
+  const goToCard = (id) => navigate(`/show/${id}`);
+
   return (
-    <Link key={info._id} to={`/show/${info._id}`}>
-      <div className="flex flex-col bg-white rounded-[20px] border-[1px] border-transparent px-[20px] py-[40px] hover:shadow-lg">
-        <div className="container">
-          <div className="h-[80px] w-[80px] mx-auto mb-[10px]">
-            <CreatedImage info={info} />
-          </div>
+    <div
+      className="flex flex-col bg-white rounded-[20px] px-[20px] py-[40px] hover:shadow-lg relative overflow-hidden cursor-pointer"
+      onClick={() => goToCard(info.id)}
+    >
+      <div className={`absolute inset-0 z-10 ${hover ? "" : "hidden"}`}>
+        <div className="absolute inset-0 bg-purple opacity-95"></div>
+        <div className="absolute bottom-0 h-1/2 w-full px-[20px]">
+          <p className="text-md font-semibold text-white">{info.description}</p>
+        </div>
+      </div>
+
+      <div
+        className="h-[100px] w-[100px] bg-white rounded-full overflow-hidden mx-auto mb-[10px] relative inset-0 z-[10]"
+        onMouseEnter={() => setHover(() => true)}
+        onMouseLeave={() => setHover(() => false)}
+      >
+        <CreatedImage info={info} />
+      </div>
+
+      <div className="flex-1 flex flex-col gap-[10px] justify-evenly w-[200px] relative z-[5]">
+        <div className="min-h-[75px]">
+          <p className="text-center text-md font-semibold">{info.name}</p>
+        </div>
+        <div className="text-sm text-center">By: {info.createdBy}</div>
+
+        <div
+          className={`w-[80px] mx-auto border-[0.5px] border-lighterpink rounded-[5px] cursor-pointer text-left px-[5px] py-[1px] flex items-center justify-center gap-[5px] ${
+            likedByCurrent ? "bg-lighterpink" : "bg-[#F5F5F5]"
+          }`}
+          onClick={handleLikeClick}
+        >
+          <button className="text-sm">❤️</button>
+          <span className="text-sm">
+            {" "}
+            {!!currentUserData ? likeState?.likes : info.likes}{" "}
+          </span>
         </div>
 
-        <div className="flex-1 flex flex-col gap-[10px] justify-evenly w-[200px]">
-          <div className="min-h-[75px]">
-            <p className="text-center text-md font-semibold">{info.name}</p>
-          </div>
-          <div className="text-sm text-center">By: {info.createdBy}</div>
-
-          <div
-            className={`w-[80px] mx-auto border-[0.5px] border-lighterpink rounded-[5px] cursor-pointer text-left px-[5px] py-[1px] flex items-center justify-center gap-[5px] ${
-              likedByCurrent ? "bg-lighterpink" : "bg-[#F5F5F5]"
-            }`}
-            onClick={handleLikeClick}
-          >
-            <button className="text-sm">❤️</button>
-            <span className="text-sm">
-              {" "}
-              {!!currentUserData ? likeState?.likes : info.likes}{" "}
+        <div className="text-sm text-center">
+          {!currentUserData ? (
+            <span className="h-[20px]">
+              <Link
+                onClick={(e) => e.stopPropagation()}
+                to="/user?form=login"
+                className="hover:underline font-semibold"
+              >
+                Sign In
+              </Link>{" "}
+              to vote!
             </span>
-          </div>
-
-          <div className="text-sm text-center">
-            {!currentUserData ? (
-              <span className="h-[20px]">
-                <Link
-                  to="/user?form=login"
-                  className="hover:underline font-semibold"
-                >
-                  Sign In
-                </Link>{" "}
-                to vote!
-              </span>
-            ) : null}
-          </div>
+          ) : null}
         </div>
-      </div>{" "}
-    </Link>
+      </div>
+    </div>
   );
 };
 
