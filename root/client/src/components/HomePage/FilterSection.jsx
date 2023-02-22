59b0060bd005835e61b75c5fc0720317f0d02ec5
guanch92 <guancheng92@gmail.com>
2023-02-22 22:32:38 +0800

    update filter

--- root/client/src/components/HomePage/FilterSection.jsx
+++ root/client/src/components/HomePage/FilterSection.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import ToggleButton from "../Reusables/ToggleButton";
+
 const axios = require("axios").default;
 const ingredientURL = {
   Bases: "/api/ingredients/base",
@@ -9,6 +9,7 @@ const ingredientURL = {
 
 const FilterSection = ({ category, selectedFilters, setSelectedFilters }) => {
   const [options, setOptions] = useState([]);
+  const [isOpen, setIsOpen] = useState(null);
 
   const getIngredientData = async (path) => {
     return axios.get(path).then(({ data }) => data);
@@ -18,29 +19,75 @@ const FilterSection = ({ category, selectedFilters, setSelectedFilters }) => {
     const response = await getIngredientData(ingredientURL[category]);
 
     const dataName = response.data.map((e) => ({
-      name: e.name, 
+      name: e.name,
       ingredientId: e._id,
-      type: category}));
+      type: category,
+    }));
     //const dataName = response.data.map((e) => e.name);
     setOptions(dataName);
   }, []);
 
-  const ingredientsDisplay = options.map((e) => (
-    <ToggleButton
-      key={e.ingredientId}
-      id={e.ingredientId}
-      category={e.type}
-      label={e.name}
-      selection={selectedFilters}
-      setSelection={setSelectedFilters}
-      input={e.type === 'Toppings' ? "checkbox" : "radio"}
-    />
-  ));
+  const handleChange = (filter) => {
+    let isRadio = filter.type !== "Toppings";
+    let index = selectedFilters.findIndex(
+      (ele) => ele.ingredientId === filter.ingredientId
+    );
+
+    isRadio ? setIsOpen(null) : null;
+
+    if (index < 0) {
+      if (isRadio) {
+        setSelectedFilters((prev) =>
+          prev.filter(({ type }) => filter.type !== type)
+        );
+      }
+      setSelectedFilters((prev) => [...prev, filter]);
+    } else {
+      setSelectedFilters((prev) => prev.filter((_, i) => index !== i));
+    }
+  };
+
+  const filters = options.map((e) => {
+    return (
+      <div
+        key={e.ingredientId}
+        onClick={() => handleChange({ ...e, id: e.ingredientId })}
+        className={`button px-[10px] py-[5px] text-xs  md:text-sm ${
+          selectedFilters.map(({ id }) => id).includes(e.ingredientId)
+            ? "bg-lightpink text-gray-700"
+            : ""
+        }`}
+      >
+        {e.name}
+      </div>
+    );
+  });
+
+  const displayedFilters = selectedFilters
+    .filter(({ type }) => type === category)
+    .map((element) => element.name)
+    .join(" + ");
 
   return (
-    <div className="my-2">
-      <div className="text-purple my-1 text-lg font-bold">{category}</div>
-      <div>{ingredientsDisplay}</div>
+    <div>
+      <div className="text-purple my-1 text-sm md:text-lg font-semibold text-center">
+        {category}
+      </div>
+      <div className="relative w-[100px] md:w-[200px] max-w-[100px] md:max-w-[250px]">
+        <div
+          className={`bg-white text-gray-700 md:min-h-[35px] px-[10px] py-[5px] rounded-[5px] button truncate text-center text-xs md:text-sm`}
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          {displayedFilters || "All"}
+        </div>
+        <div
+          className={`absolute z-10 mt-[1px] bg-white w-full rounded-[10px] overflow-hidden ${
+            isOpen ? "" : "hidden"
+          }`}
+        >
+          {filters}
+        </div>
+      </div>
     </div>
   );
 };
